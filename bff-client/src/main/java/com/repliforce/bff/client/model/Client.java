ab02890c2c0e58b706386c3b5b97543494480196
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-07-14 22:29:42 -0300

    Project Client Schedule - Model Client created.

---
+++ bff-client/src/main/java/com/repliforce/bff/client/model/Client.java
@@ -0,0 +1,20 @@
+package com.repliforce.bff.client.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate registeredAt;
+}
